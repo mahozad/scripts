a89637c124880c691206da41b2e3543b1475716f
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-02-22 14:38:38 +0330

    Update test dependencies and their usage in code
    - Comment Kotlin test and MockK dependencies
    - Add AssertJ dependency and use it in tests

--- testing-kotlin-script/test/ScriptTest.kt
+++ testing-kotlin-script/test/ScriptTest.kt
@@ -1,11 +1,13 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.test.assertEquals
 
 class ScriptTest {
 
     @Test
     fun firstTest() {
         val os = Script_main(emptyArray()).getEnvironmentVariable("OS")
-        assertEquals("Windows_NT", os)
+        assertThat(os).isEqualTo("Windows_NT") // Using AssertJ assertion
+        assertEquals("Windows_NT", os) // Using JUnit assertion
     }
 }
