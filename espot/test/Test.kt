d2cfe5c04248d4ce5d09240d13d468b8432bd498
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-02-25 15:42:03 +0330

    Add first real unit tests

--- espot/test/Test.kt
+++ espot/test/Test.kt
@@ -1,10 +1,34 @@
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.nio.file.Files
+import java.nio.file.Path
+import java.time.Duration
+import java.time.Instant
 
 class Test {
 
     @Test
-    fun pass() {
-        assertThat(Espot_main(emptyArray()).root).isNotNull()
+    fun `script should be fast`() {
+        val root = Path.of("test/hierarchy-1/")
+        val result = Path.of("result.txt")
+        val args = arrayOf("${root.toAbsolutePath()}", "${result.toAbsolutePath()}")
+
+        val startTime = Instant.now()
+        Espot_main(args)
+        val duration = Duration.between(startTime, Instant.now())
+
+        assertThat(duration).isLessThan(Duration.ofMillis(50))
+    }
+
+    @Test
+    fun `check result for hierarchy 1`() {
+        val root = Path.of("test/hierarchy-1/")
+        val result = Path.of("result.txt")
+        val args = arrayOf("${root.toAbsolutePath()}", "${result.toAbsolutePath()}")
+        val expected = Files.readString(Path.of("test/expected-result-1.txt"))
+
+        Espot_main(args)
+
+        assertThat(Files.readString(result)).isEqualTo(expected)
     }
 }
