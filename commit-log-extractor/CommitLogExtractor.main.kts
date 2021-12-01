78efcc404f8b03d8dc278f01da4087b1217836c8
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-12-01 19:20:06 +0330

    Refactor code

--- commit-log-extractor/CommitLogExtractor.main.kts
+++ commit-log-extractor/CommitLogExtractor.main.kts
@@ -3,17 +3,14 @@
 @file:DependsOn("com.github.mfathi91:persian-date-time:4.1.0")
 
 import com.github.mfathi91.time.PersianDate
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 import java.nio.file.Path
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
-import java.time.format.DateTimeFormatter.ofPattern
 import java.time.format.DecimalStyle
-import java.time.temporal.ChronoUnit.DAYS
+import java.time.temporal.ChronoUnit
 import java.util.*
 
 val targetDay = LocalDateTime.of(2020, 8, 13, 0, 0)
@@ -21,18 +18,22 @@ val commandBase = "git log --pretty=format:\"%%s\" --after=\"%s\" --before=\"%s\
 val outputDirectory = "./"
 
 val start = targetDay.format(ISO_LOCAL_DATE_TIME)
-val end = targetDay.plus(1, DAYS).format(ISO_LOCAL_DATE_TIME)
+val end = targetDay.plus(1, ChronoUnit.YEARS).format(ISO_LOCAL_DATE_TIME)
 val command = String.format(commandBase, start, end)
 val process = Runtime.getRuntime().exec(command)
 
 val locale = Locale.forLanguageTag("fa")
-val formatter = ofPattern("yyyy-MM-dd").localizedBy(locale).withDecimalStyle(DecimalStyle.of(locale))
+val formatter = DateTimeFormatter
+    .ofPattern("yyyy-MM-dd")
+    .localizedBy(locale)
+    .withDecimalStyle(DecimalStyle.of(locale))
 val persianDate = PersianDate.fromGregorian(LocalDate.from(targetDay))
 val date = formatter.format(persianDate)
 val fileName = "$date.txt"
 val outputPath = Path.of(outputDirectory + fileName)
 
-val logs = BufferedReader(InputStreamReader(process.inputStream, StandardCharsets.UTF_8))
+val logs = process
+    .inputReader()
     .lineSequence()
     .joinToString("\r\n")
 
